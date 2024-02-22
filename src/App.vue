4327b036dab395d9d160563f10df4b0703149248
tarun5354 <tarungarigipati@gmail.com>
2024-02-22 19:02:26 +0530

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,118 @@
+<template>
+  <h2>List data from web site</h2>
+  <SinglePost 
+  v-for="(item,index) in data"
+  
+  
+  :name="item.Moviename" 
+  :released="item.releaseday" 
+  :introduction="item.description" 
+  :collection="item.collections"
+  :position="index"
+  :imagespic="item.images"
+  v-on:delecteCard="delecteCard"
+  />
+  
+  
+  </template>
+  <script setup>
+  import SinglePost from './components/SinglePost.vue';
+import {ref} from 'vue';
+let data=ref([
+  {
+    Moviename:"jaishihaa",
+    releaseday:"21-11-2024",
+    description:"Dive into the neon-lit streets of  a neo-noir film that combines elements of mystery and science fiction. Follow a detective as he unravels a web of conspiracy in a futuristic city.",
+    collections:213263763278 ,
+    images:"https://static.toiimg.com/thumb/msid-103656989,width-1280,height-720,resizemode-4/103656989.jpg"
+    
+
+
+
+
+
+},
+
+{
+    Moviename:"guntur-karam",
+    releaseday:"12-09-2024",
+    description:"unravels the secrets of a mysterious city. Join the adventure as a detective tries to solve a series of perplexing crimes, revealing the hidden layers of Guntur-Karam.",
+    collections:287 ,
+    images:"https://static.toiimg.com/thumb/msid-103656989,width-1280,height-720,resizemode-4/103656989.jpg"
+    
+
+
+
+
+
+},
+
+{
+Moviename:"Inception Reimagined",
+    releaseday:"14-12-2024",
+    description:"A mind-bending heist movie directed by visionary filmmaker Christopher Nolan. Dive into dreams within dreams as a skilled thief navigates the complexity of the subconscious.",
+    collections:287  ,
+    images:"https://static.toiimg.com/thumb/msid-103656989,width-1280,height-720,resizemode-4/103656989.jpg"
+    
+
+
+
+
+
+},
+
+{
+Moviename:"salaar",
+    releaseday:"14-12-2024",
+    description:" unfolds the gripping tale of a lone warrior on a quest for justice. With intense action sequences and a powerful narrative, this film explores the essence of honor and sacrifice.",
+    collections:287  ,
+    images:"https://static.toiimg.com/thumb/msid-103656989,width-1280,height-720,resizemode-4/103656989.jpg"
+    
+
+
+
+
+
+},
+{
+Moviename:"The Elysian Chronicles",
+    releaseday:"14-12-2024",
+    description:"Enter the mythical world of  where ancient prophecies and mystical creatures come to life. A group of heroes must unite to save their realm from an impending darkness.",
+    collections:287  ,
+    images:"https://static.toiimg.com/thumb/msid-103656989,width-1280,height-720,resizemode-4/103656989.jpg"
+    
+
+
+
+
+
+},{
+Moviename:"Ocean's Riddle",
+    releaseday:"14-12-2024",
+    description:"is a suspenseful thriller that takes you into the world of high-stakes heists. A group of skilled individuals must outsmart the authorities and navigate intricate plans to achieve the ultimate score. ",
+    collections:287  ,
+    images:"https://static.toiimg.com/thumb/msid-103656989,width-1280,height-720,resizemode-4/103656989.jpg"
+    
+
+
+
+
+
+}
+
+
+
+
+])
+
+function delecteCard(position){
+  data.value.splice(position,1)
+}
+</script>
+<style scoped>
+  h2 {
+    text-align: center;
+    color: #353434;
+  }
+
+  </style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="container">
+
+        <ul>
+            <li><div><img :src="imagespic"/></div></li>
+              <li>Moviename:  {{ name }}</li>
+               <li>releasedday:  {{ released }}</li>
+               <li>description: <span>{{ introduction }}</span></li>
+               <li>{{ collection }}</li>    
+               <li>{{ imagespic }}</li> 
+              
+
+
+        </ul>
+        <button @click="$emit('delecteCard',position)">delete the inform</button>
+    </div>
+</template>
+<script setup>
+defineProps({
+    name:String,
+    released:String,
+    introduction:String,
+    collection:Number,
+    position:Number,
+    imagespic:String
+
+
+
+           
+
+
+
+
+
+})
+
+</script>
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 10px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+    margin-bottom: 10px;
+}
+li div {
+    text-align: center;
+}
+img {
+  border-radius: 5px;
+  margin-top: 10px;
+  height: 300px;
+  justify-content: center;
+  
+  text-align: center;
+  margin:0 auto;
+}
+
+button {
+  grid-column: span 2; /* Button spans two columns */
+  background-color: #dc3545;
+  color: #fff;
+  border: none;
+  padding: 8px 12px;
+  cursor: pointer;
+  border-radius: 3px;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #c82333;
+}
+
+span {
+  font-weight: bold;
+  display: block;
+}
+</style>
+
